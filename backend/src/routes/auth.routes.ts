0d963ceab18c84d7c4e62ef7a1b20eeb6a5428bb
vibrant1153 <bytheconsentofallah@gmail.com>
2026-05-03 22:00:31 +0300

    auth route

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { register, login, logout, getMe } from '../controllers/auth.controller.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', authMiddleware, logout);
+router.get('/me', authMiddleware, getMe);
+
+export default router;
